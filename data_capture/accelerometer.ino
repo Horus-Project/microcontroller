79948250d0e0388e4042e5ca1679d8c3c99c18a6
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-01-25 23:29:36 +0100

    modified data capture program to don't use global variables for acc values but pointers to local variables in the main script

--- data_capture/accelerometer.ino
+++ data_capture/accelerometer.ino
@@ -5,6 +5,9 @@
 
 #define CALIBRATION_DURATION 3000 // calibration is 3s long
 
+uint8_t bytes = 6;
+
+
 void accelerometer_setup() {
   Serial.println("setting up accelerometer");
   Wire.begin(21,22); // D2 (GPIO12) = SDA | D1(GPIO22)=SCL | default I2C ports
@@ -16,19 +19,19 @@ void accelerometer_setup() {
 
 // Puts the lectures of the accelerometer
 // in the global vairables
-void read_acc() {
+void read_acc(int16_t* ax, int16_t* ay, int16_t* az, int16_t* temp, int16_t* gx, int16_t* gy, int16_t* gz) {
   Wire.beginTransmission(MPU_ADDR);
   Wire.write(0x3B); // ACCEL_XOUT_H
   Wire.endTransmission();
-  Wire.requestFrom(MPU_ADDR,14,true);
+  Wire.requestFrom(MPU_ADDR,14,1);
 
-  a_x = Wire.read()<<8|Wire.read();
-  a_y = Wire.read()<<8|Wire.read();
-  a_z = Wire.read()<<8|Wire.read();
+  *ax = (((int16_t)Wire.read())<<8)|Wire.read();
+  *ay = (((int16_t)Wire.read())<<8)|Wire.read();
+  *az = (((int16_t)Wire.read())<<8)|Wire.read();
 
-  temp = Wire.read()<<8|Wire.read();
+  *temp = Wire.read()<<8|Wire.read();
 
-  g_x = Wire.read()<<8|Wire.read();
-  g_y = Wire.read()<<8|Wire.read();
-  g_z = Wire.read()<<8|Wire.read();
+  *g_x = Wire.read()<<8|Wire.read();
+  *g_y = Wire.read()<<8|Wire.read();
+  *g_z = Wire.read()<<8|Wire.read();
 }
