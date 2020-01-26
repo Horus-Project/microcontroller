867d7762292ffadec19d5d637377d319d38efcf5
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-01-26 11:47:14 +0100

    modified main program to do not use global variables to store sensor data but pointers instead

--- main/accelerometer.ino
+++ main/accelerometer.ino
@@ -14,42 +14,50 @@ void accelerometer_setup() {
   Wire.endTransmission(true);
 }
 
-void calibrate() {
+void calibrate(int16_t* maxax, int16_t* maxay, int16_t* maxaz) {
   Serial.println("calibrating");
+
+  int16_t ax, ay, az, tmp, gx, gy, gz;
   t = millis();
   unsigned long calibration_end = t + CALIBRATION_DURATION;
   while(t < calibration_end){
     // read values, keep calibration maxes
-    read_acc();
-    if (a_x > max_x) {
-      max_x = a_x;
+    read_acc(&ax, &ay, &az, &tmp, &gx, &gy, &gz);
+    if (ax > *maxax) {
+      *maxax = ax;
     }
-    if (a_y > max_y) {
-      max_y = a_y;
+    if (ay > *maxay) {
+      *maxay = ay;
     }
-    if (a_z > max_z) {
-      max_z = a_z;
+    if (az > *maxaz) {
+      *maxaz = az;
     }
     delay(10);
     t = millis();
   }
-  String calibration_result = "max_x: "+String(max_x)+" max_y: "+String(max_y)+" max_z: "+String(max_z);
+  String calibration_result = "max_x: "+String(*maxax)+" max_y: "+String(*maxay)+" max_z: "+String(*maxaz);
   Serial.println(calibration_result);
 }
 
 // Puts the lectures of the accelerometer
 // in the global vairables
-void read_acc() {
+void read_acc(int16_t* ax, int16_t* ay, int16_t* az, int16_t* temp, int16_t* gx, int16_t* gy, int16_t* gz) {
   Wire.beginTransmission(MPU_ADDR);
   Wire.write(0x3B); // ACCEL_XOUT_H
   Wire.endTransmission();
-  Wire.requestFrom(MPU_ADDR,6,true);
+  Wire.requestFrom(MPU_ADDR,14,true);
 
-  a_x = Wire.read()<<8|Wire.read();
-  a_y = Wire.read()<<8|Wire.read();
-  a_z = Wire.read()<<8|Wire.read();
+  *ax = Wire.read()<<8|Wire.read();
+  *ay = Wire.read()<<8|Wire.read();
+  *az = Wire.read()<<8|Wire.read();
+
+  *temp = Wire.read()<<8|Wire.read();
+  
+  *gx = Wire.read()<<8|Wire.read();
+  *gy = Wire.read()<<8|Wire.read();
+  *gz = Wire.read()<<8|Wire.read();
 
   if(DEBUG) {
-    values = "X:"+String(a_x); + " Y:"+String(a_y)+", Z:"+String(a_z);
+    values = "X:"+String(*ax) + " Y:"+String(*ay)+", Z:"+String(*az);
   }
 }
