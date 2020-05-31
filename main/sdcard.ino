3ecd804a268914d06673433ddf46ad8c35b1efa7
Pablo <pablo.riutort@smachteam.com>
2020-05-31 20:51:10 +0200

    Add error and blinking codes
    main.ino:
        Add Linux System error codes
        Add blink function to control microcontroller integrated led
        Add blink behaviours:
    	WiFi connection: 2 short blinks for successful connection
    	SD card errors: 2 or 4 long blinks for SD card error
    
    sdcard.ino: refactor SD_Setup and append_file to return int

--- main/sdcard.ino
+++ main/sdcard.ino
@@ -3,34 +3,37 @@
 #include "SD.h"
 #include "SPI.h"
 
-void SD_setup() {
+int SD_setup() {
   Serial.println("setting up SD card module");
   if(!SD.begin()){
     Serial.println("Card Mount Failed");
-    return;
+    return EIO;
   }
+  return 0;
 }
 
-void append_file(fs::FS &fs, const char * path, const char * message) {
+int append_file(fs::FS &fs, const char * path, const char * message) {
+    int error = 0;
     if (SD_DEBUG) {
       Serial.printf("Appending to file: %s\n", path);
     }
-
     File file = fs.open(path, FILE_APPEND);
     if(!file){
-        if (SD_DEBUG) {
-            Serial.println("Failed to open file for appending");
-        }
-        return;
+      if (SD_DEBUG) {
+        Serial.println("ERROR: Failed to open file");
+      }
+      return ENOENT;
     }
-    if(file.print(message)){
-        if (SD_DEBUG) {
-            Serial.println("Message appended");
-        }
-    } else {
-        if (SD_DEBUG) {
-            Serial.println("Append failed");
-        }
+    if (!file.print(message)){
+      error = EIO;
+    }
+    if (SD_DEBUG) {
+      if (error) {
+        Serial.println("ERROR: Append failed");
+      } else {
+        Serial.println("Message appended");
+      }
     }
     file.close();
+    return error;
 }
